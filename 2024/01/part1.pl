a86f98f91a11da4acf9345d3562f15e27e2ca0fc
Tim Brown <tim@air.local>
2024-12-09 19:48:28 +0000

    added tests to part1.pl and refactored

--- 2024/01/part1.pl
+++ 2024/01/part1.pl
@@ -2,37 +2,61 @@
 
 use strict;
 use Data::Dumper;
-my $i = 0;
 my $position;
 my $highest;
+my @col1;
+my @col2;
 
-# populate data structures
-while(<>){
-  m/(\d+)\s+(\d+)/ or die "expecting 2 integers, but read $_";
-  $i++;
-  $position->{column_1}->{$i}->{value} = $1;
-  $highest->{column_1} = $1 if $1 > $highest->{column_1};
+sub main{
+  # populate data structures
+  while(<>){
+    m/(\d+)\s+(\d+)/ or die "expecting 2 integers, but read $_";
+    push @col1, $1;
+    push @col2, $2;
+  }
 
-  $position->{column_2}->{$i}->{value} = $2;
-  $highest->{column_2} = $2 if $2 > $highest->{column_2};
+  print 'Total: ', sum_differences(build_data_for(\@col1), max_value_of(\@col1), build_data_for(\@col2), max_value_of(\@col2)), "\n";
 }
 
-my $total = 0;
-my $col1_pointer = 0;
-my $col2_pointer = 0;
+sub sum_differences {
+  my ($column_1_data, $column_1_max, $column_2_data, $column_2_max) = @_;
 
-for my $j (1..$i){
-  my $position_of_next_from_col1 = find_next_position($position->{column_1}, $highest->{column_1});
-  my $position_of_next_from_col2 = find_next_position($position->{column_2}, $highest->{column_2});
+  my $total = 0;
+  my $data_length = scalar keys %$column_1_data;
+  my $col1_pointer = 0;
+  my $col2_pointer = 0;
+  
+  # iterate over data to sum the result
+  for my $j (1..$data_length){
+    my $position_of_next_from_col1 = find_next_position($column_1_data, $column_1_max);
+    my $position_of_next_from_col2 = find_next_position($column_2_data, $column_2_max);
 
-  #print "Col 1: $position_of_next_from_col1, Col 2: $position_of_next_from_col2 ", abs($position_of_next_from_col1 - $position_of_next_from_col2), "\n";
-  $total += abs($position_of_next_from_col1 - $position_of_next_from_col2);
+  # #print "Col 1: $position_of_next_from_col1, Col 2: $position_of_next_from_col2 ", abs($position_of_next_from_col1 - $position_of_next_from_col2), "\n";
+    $total += abs($position_of_next_from_col1 - $position_of_next_from_col2);
+  }
+
+  return $total;
 }
-print "Total: $total\n";
 
+sub max_value_of{
+  my $array = shift;
+  my @sorted = sort {$b <=> $a} @$array;
+  return $sorted[0];
+}
 
+sub build_data_for {
+  my $integers = shift;
+  my $data;
+  my $i = 0;
 
-sub find_next_position(){
+  for my $value (@$integers){
+    $i++;
+    $data->{$i}->{value} = $value;
+  }
+  return $data;
+}
+
+sub find_next_position{
   my ($column_data, $highest) = @_;
 
   my $position_of_lowest = 0;
@@ -48,5 +72,38 @@ sub find_next_position(){
   }
 
   $column_data->{$position_of_lowest}->{ignore} = 1;
+  die "data is exhausted\n" unless $position_of_lowest;
+
   return $position_of_lowest;
+}
+
+# tests can be run like...
+# ./part1.pl test
+# otherwise main() is invoked
+($ARGV[0] =~ m/test/)? tests() : main();
+
+sub tests{
+  require Test::More;
+  import Test::More tests => 12;
+  use Test::Exception;
+
+  my @integers = (20, 9, 2, 18, 7);
+
+  my $data = build_data_for(\@integers);
+  is($data->{1}->{value}, 20, 'data built correctly');
+  is($data->{2}->{value}, 9,  'data built correctly');
+  is($data->{3}->{value}, 2,  'data built correctly');
+  is($data->{4}->{value}, 18, 'data built correctly');
+  is($data->{5}->{value}, 7,  'data built correctly');
+
+  is(max_value_of($data), 20, 'max_value_for');
+
+  is(find_next_position($data, 20), 3, '1st call returns 3');
+  is(find_next_position($data, 20), 5, '2nd call returns 5');
+  is(find_next_position($data, 20), 2, '3rd call returns 2');
+  is(find_next_position($data, 20), 4, '4th call returns 4');
+  is(find_next_position($data, 20), 1, '5th call returns 1');
+  dies_ok {find_next_position($data, 20)} '6th call dies';
+
+  
 }
