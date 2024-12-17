1d190087ca62da49a59667e4a95f68cadc775eb3
Tim Brown <tim@air.local>
2024-12-17 15:47:59 +0000

    fixed part1.pl

--- 2024/06/part1.pl
+++ 2024/06/part1.pl
@@ -5,8 +5,9 @@ use Data::Dumper;
 use lib '.';
 use Map;
 
-my @lines = join '', <>;
-my $MAP = Map->new_from_string(\@lines);
+my $lines = join '', <>;
+my $MAP = Map->new_from_string($lines);
 
-print Dumper $MAP;
+#print Dumper $MAP;
 
+$MAP->show();
