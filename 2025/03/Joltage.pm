21ff1f536a285645c965ca71ef2fa07a65421c3c
Tim Brown <tim@air.local>
2025-12-22 19:02:01 +0000

    completed 2025/03

--- 2025/03/Joltage.pm
+++ 2025/03/Joltage.pm
@@ -34,27 +34,25 @@ sub jolt {
 
 sub twelve_digit_jolt {
   my ($self, $number) = @_;
-  my $len = length "$number";
-  warn $len;
+  my $length = length "$number";
 
   my @digit = split '', "$number";
   my @result;
   my $position = 0;
 
-  warn Dumper \@digit;
+  my $target_length = 12;
+  my $remaining_items = 12;
 
-  for (0 .. 11){
-    my $end_index = $position + 10;
-    $end_index = $len if $end_index > $len;
-
+  for (0 .. $target_length - 1){
     my $start_index = $position;
+    my $end_index = $length - $remaining_items;
+
     my $slice_length = $end_index - $start_index;
 
-    my $pair = $self->highest_and_place(@digit[$position ..  $end_index]);
+    my $pair = $self->highest_and_place(@digit[$start_index ..  $end_index]);
     push @result, $pair->[0];
     $position += $pair->[1];
-    warn "Position is now $position\n";
-    # warn Dumper \@result;
+    $remaining_items--;
   }
 
   return $result[0].$result[1].$result[2].$result[3].$result[4].$result[5].$result[6].$result[7].$result[8].$result[9].$result[10].$result[11]
@@ -74,7 +72,6 @@ sub highest_and_place {
     $value = $d;
     $position = $i;
   }
-  warn ">>>>> $value ($position)";
   return [$value, $position];
 }
 
--- 2025/03/joltage.t
+++ 2025/03/joltage.t
@@ -16,8 +16,9 @@ is_deeply ($J->highest_and_place(1,2,3,4,5,1,2), [5,5], 'highest_and_place(1,2,3
 is_deeply ($J->highest_and_place(5,5,5,5,5,1,2), [5,1], 'highest_and_place(5,5,5,5,5,1,2) is [5,1]');
 is_deeply ($J->highest_and_place(5,5,5,5,5,1,9), [9,7], 'highest_and_place(5,5,5,5,5,1,9) is [9,7]');
 
-is ($J->twelve_digit_jolt('987654321111111'), '987654321111', 'jolt 987654321111111 is 987654321111');
-is ($J->twelve_digit_jolt('811111111111119'), '811111111119', 'jolt 811111111111119 is 811111111119');
-
+is ($J->twelve_digit_jolt('987654321111111'), '987654321111', 'twelve_digit_jolt 987654321111111 is 987654321111');
+is ($J->twelve_digit_jolt('811111111111119'), '811111111119', 'twelve_digit_jolt 811111111111119 is 811111111119');
+is ($J->twelve_digit_jolt('234234234234278'), '434234234278', 'twelve_digit_jolt 234234234234278 is 434234234278');
+is ($J->twelve_digit_jolt('818181911112111'), '888911112111', 'twelve_digit_jolt 818181911112111 is 888911112111');
 
 done_testing();
